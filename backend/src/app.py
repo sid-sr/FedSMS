25f97f6fdc36ca15a9899a77795088c34198bd1a
Siddharth Sriraman <siddharth18150@cse.ssn.edu.in>
2021-12-02 15:01:55 +0530

    changed REST API folder structure

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,28 +1,16 @@
-
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
 from flask import Flask
+from flask_restful import Api
 import os
 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
+from resources.hello import Hello
+
 app = Flask(__name__, static_folder='../build', static_url_path='/')
+api = Api(app)
 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
-
-
-@app.route('/api/hello')
-def hello_world():
-    return 'Hello World'
-
+api.add_resource(Hello, '/api/hello')
 
 # main driver function
 if __name__ == '__main__':
-
-    # run() method of Flask class runs the application
-    # on the local development server.
     host = os.environ.get('SERVER_ADDR', '127.0.0.1')
     port = os.environ.get('PORT', '5000')
     app.run(host=host, port=port, debug=True)
