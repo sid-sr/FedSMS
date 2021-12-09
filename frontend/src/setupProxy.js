ed27c54c126fd1c2733cd5cdefc05529e081dab8
Siddharth Sriraman <siddharth18150@cse.ssn.edu.in>
2021-12-09 18:23:54 +0530

    added s3 fetch model route

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -6,6 +6,15 @@ const HOST = process.env.HOST_ADDR || 'localhost';
 
 module.exports = function (app) {
   app.use(
+    '/api/download',
+    createProxyMiddleware({
+      target: process.env.S3_URL,
+      changeOrigin: true,
+      // remove the api/download part
+      pathRewrite: { '^/api/download': '' },
+    })
+  );
+  app.use(
     '/api',
     createProxyMiddleware({
       target: `http://${HOST}:5000/`,
