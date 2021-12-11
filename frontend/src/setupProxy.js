22e1f79838482bdea532482c6e8137b274109ffb
Siddharth Sriraman <siddharth18150@cse.ssn.edu.in>
2021-12-12 00:39:22 +0530

    setup docker for tf, .env, changed env var in frontend

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -5,15 +5,17 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const HOST = process.env.HOST_ADDR || 'localhost';
 
 module.exports = function (app) {
-  app.use(
-    '/api/download',
-    createProxyMiddleware({
-      target: process.env.S3_URL,
-      changeOrigin: true,
-      // remove the api/download part
-      pathRewrite: { '^/api/download': '' },
-    })
-  );
+  if (process.env.S3_URL) {
+    app.use(
+      '/api/download',
+      createProxyMiddleware({
+        target: process.env.S3_URL,
+        changeOrigin: true,
+        // remove the api/download part
+        pathRewrite: { '^/api/download': '' },
+      })
+    );
+  }
   app.use(
     '/api',
     createProxyMiddleware({
