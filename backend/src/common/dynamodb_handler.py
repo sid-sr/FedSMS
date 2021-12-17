2e1466c4d8778b441636b9caad0e97d9b8eda491
Siddharth Sriraman <siddharth18150@cse.ssn.edu.in>
2021-12-17 18:22:31 +0530

    formatting, removed unused imports, unused env variables

--- backend/src/common/dynamodb_handler.py
+++ backend/src/common/dynamodb_handler.py
@@ -2,20 +2,20 @@ import boto3
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-import sys
-import ast
 import json
 import decimal
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-AWS_ACCESS_KEY_ID     = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-REGION_NAME           = 'us-east-2'
+AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY', '')
+AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY', '')
+REGION_NAME = 'us-east-2'
 
-# Helper class to convert a DynamoDB item to JSON.
+
 class DecimalEncoder(json.JSONEncoder):
+    '''Helper class to convert a DynamoDB item to JSON.'''
+
     def default(self, o):
         if isinstance(o, decimal.Decimal):
             if o % 1 > 0:
@@ -24,19 +24,19 @@ class DecimalEncoder(json.JSONEncoder):
                 return int(o)
         return super(DecimalEncoder, self).default(o)
 
+
 client = boto3.client(
     'dynamodb',
-    aws_access_key_id     = AWS_ACCESS_KEY_ID,
-    aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
-    region_name           = REGION_NAME,
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_KEY,
+    region_name=REGION_NAME,
 )
 resource = boto3.resource(
     'dynamodb',
-    aws_access_key_id     = AWS_ACCESS_KEY_ID,
-    aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
-    region_name           = REGION_NAME,
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_KEY,
+    region_name=REGION_NAME,
 )
 
 ConfigTable = resource.Table('config')
 ClientModelTable = resource.Table('client_models')
-
--- backend/src/common/federated.py
+++ backend/src/common/federated.py
@@ -1,40 +1,46 @@
-import os
-from flask import request,abort
-from flask_restful import Resource
-from common.dynamodb_handler import ConfigTable,DecimalEncoder
+from common.dynamodb_handler import ConfigTable, DecimalEncoder
 import json
 import ast
-from decimal import Decimal
-from datetime import datetime
+
 
 def incrementModelIndex():
-  #get the current config info from dynamodb
-  current_config = ConfigTable.get_item(Key = {'id': 0})  
-  current_config = ast.literal_eval((json.dumps(current_config['Item'], cls=DecimalEncoder)))
-  result={}
-  try:
-    if(current_config['modelIndex']==current_config['clients']-1):  #check if it is the last client in that round
-      #increment the modelindex(becomes 0) and also increment rounds completed
-      response=ConfigTable.update_item(
-        Key = {'id': 0},
-        AttributeUpdates={
-            'roundsCompleted': {'Value'  : current_config['roundsCompleted']+1},
-            'modelIndex': {'Value'  : (current_config['modelIndex']+1)%current_config['clients']}
-        },
-        ReturnValues="UPDATED_NEW"
-      )  
-      #insert call to aggregate
+    # get the current config info from dynamodb
+    current_config = ConfigTable.get_item(Key={'id': 0})
+    current_config = ast.literal_eval(
+        (json.dumps(current_config['Item'], cls=DecimalEncoder)))
+    result = {}
+    try:
+        # check if it is the last client in that round
+        if current_config['modelIndex'] == current_config['clients'] - 1:
+            # increment the modelindex(becomes 0) and also increment rounds completed
+            response = ConfigTable.update_item(
+                Key={'id': 0},
+                AttributeUpdates={
+                    'roundsCompleted': {
+                        'Value': current_config['roundsCompleted']+1
+                    },
+                    'modelIndex': {
+                        'Value': (current_config['modelIndex']+1) % current_config['clients']
+                    }
+                },
+                ReturnValues="UPDATED_NEW"
+            )
+            # insert call to aggregate
+        else:
+            # round not completed so increment only modelIndex
+            response = ConfigTable.update_item(
+                Key={'id': 0},
+                AttributeUpdates={
+                    'modelIndex': {
+                        'Value': (current_config['modelIndex']+1) % current_config['clients']
+                    }
+                },
+                ReturnValues="UPDATED_NEW"
+            )
+    except Exception as e:
+        result['status'] = 'Error'
+        return result
     else:
-      #round not completed so increment only modelIndex
-      response=ConfigTable.update_item(
-        Key = {'id': 0},
-        AttributeUpdates={'modelIndex': {'Value'  : (current_config['modelIndex']+1)%current_config['clients']}},
-        ReturnValues="UPDATED_NEW"
-      )
-  except Exception as e:
-    result['status']='Error'
-    return result
-  else:
-    result['status']='New value: '+str(response['Attributes']['modelIndex'])
-    return result
-
+        result['status'] = 'New value: ' + \
+            str(response['Attributes']['modelIndex'])
+        return result
