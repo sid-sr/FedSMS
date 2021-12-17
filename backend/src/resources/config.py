52cd861454a9a0ffe7423421e64efa0f91f07699
Sneha Sriram Kannan <52703275+snehask7@users.noreply.github.com>
2021-12-17 16:35:52 +0530

    Added increment modelIndex function
    Added function to increment model index as well as round, modified the edit config function such that edit when a round is in progress is not allowed

--- backend/src/resources/config.py
+++ backend/src/resources/config.py
@@ -11,30 +11,36 @@ import pytz
 class Config(Resource):
     def get(self):
       #get the record in the config table
-      response = ConfigTable.get_item(Key = {'id': 0})  
+      current_config = ConfigTable.get_item(Key = {'id': 0})  
       #convert the decimal objects from dynamodb
-      decimal_encoded = ast.literal_eval((json.dumps(response['Item'], cls=DecimalEncoder)))
+      decimal_encoded = ast.literal_eval((json.dumps(current_config['Item'], cls=DecimalEncoder)))
       return decimal_encoded
 
     def put(self):
-      json_request = request.get_json(force=True)
-      json_request = json.loads(json.dumps(json_request ), parse_float=Decimal)
-      try:
-        response=ConfigTable.update_item(
-          Key = {'id': 0},
-          AttributeUpdates={
-              'strategy': {'Value'  : json_request['strategy']},
-              'fraction': {'Value'  : json_request['fraction']},
-              'clients': {'Value'  : json_request['clients']},
-              'qfedAvg_q': {'Value'  : json_request['qfedAvg_q']},
-              'qfedAvg_l': {'Value'  : json_request['qfedAvg_l']},
-              'lastUpdatedAt': {'Value':str(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))}
-          },
-          ReturnValues="UPDATED_NEW"
-        )
-      except Exception as e:
-        abort(400, 'Invalid request body')
+      #edit certain config fields by taking input from the dashboard
+      current_config = ConfigTable.get_item(Key = {'id': 0}) 
+      decimal_encoded = ast.literal_eval((json.dumps(current_config['Item'], cls=DecimalEncoder))) 
+      if(decimal_encoded['modelIndex']==0): #if there is a round in progress, config info cannot be updated
+        json_request = request.get_json(force=True)
+        json_request = json.loads(json.dumps(json_request ), parse_float=Decimal)
+        try:
+          response=ConfigTable.update_item(
+            Key = {'id': 0},
+            AttributeUpdates={
+                'strategy': {'Value'  : json_request['strategy']},
+                'fraction': {'Value'  : json_request['fraction']},
+                'clients': {'Value'  : json_request['clients']},
+                'qfedAvg_q': {'Value'  : json_request['qfedAvg_q']},
+                'qfedAvg_l': {'Value'  : json_request['qfedAvg_l']},
+                'lastUpdatedAt': {'Value':str(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))}
+            },
+            ReturnValues="UPDATED_NEW"
+          )
+        except Exception as e:
+          abort(400, 'Invalid request body')
+        else:
+          return "Updated"
       else:
-        return "config put request"
+        abort(400, 'Round in progress, cannot update configuration parameters')
 
 
