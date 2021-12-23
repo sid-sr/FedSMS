9d52b1b046d87f86cab898f0c3c51c6e127bb1e3
Siddharth Sriraman <siddharth18150@cse.ssn.edu.in>
2021-12-23 13:48:42 +0530

    added dummy training data, trained model in frontend

--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -2,61 +2,104 @@ import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import { useState } from 'react';
 import { FaArrowLeft, FaCheckCircle } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
 import ClipLoader from 'react-spinners/ClipLoader';
 import PulseLoader from 'react-spinners/PulseLoader';
 import '../styles/settings.css';
 import * as tf from '@tensorflow/tfjs';
+import axios from 'axios';
 
 function Settings() {
-  const navigate = useNavigate();
-
   const [fetchText, setFetchText] = useState('Fetch');
   const [uploadText, setUploadText] = useState('Upload');
   const [messageCount, setMessageCount] = useState(10);
   const [fetchModelText, setFetchModelText] = useState('Fetch Model');
+  const [trainModelText, setTrainModelText] = useState('Train Model');
+  const [trainStats, setTrainStats] = useState({
+    numMessages: 0,
+    trainLoss: 0,
+    trainAcc: 0,
+  });
   const [model, setModel] = useState(null);
+  const [trainData, setTrainData] = useState([]);
 
   const onSlide = (value) => {
     setMessageCount(value);
   };
 
-  function timeout(delay) {
-    return new Promise((res) => setTimeout(res, delay));
-  }
-
   async function loadModel() {
     setFetchModelText('Fetching');
     const model = await tf.loadLayersModel('/api/download/model.json');
     model.summary();
+    model.compile({
+      loss: 'binaryCrossentropy',
+      optimizer: tf.train.adam(0.01), // This is a standard compile config
+      metrics: ['accuracy'],
+    });
     setModel(model);
     setFetchModelText('Fetched');
+  }
+
+  async function train() {
+    setTrainModelText('Training');
+    if (trainData.length === 0 || !model) return;
+    const x_train = [],
+      y_train = [];
+
+    for (const row of trainData) {
+      x_train.push(row['embedding']);
+      y_train.push(1 * row['spam']);
+    }
+
+    const xs = tf.tensor2d(x_train, [
+      trainData.length,
+      trainData[0]['embedding'].length,
+    ]);
+    const ys = tf.tensor1d(y_train);
+
+    await model.fit(xs, ys, {
+      epochs: 1,
+      batchSize: 32,
+      callbacks: {
+        onEpochEnd: async (epoch, logs) => {
+          console.log('Epoch: ' + epoch + ' Loss: ' + logs.loss);
+        },
+      },
+    });
+    const stats = model.evaluate(xs, ys);
+    console.log(stats[0].dataSync()[0], stats[1].dataSync()[0]);
+    setTrainStats({
+      numMessages: trainData.length,
+      trainLoss: Math.round(stats[0].dataSync()[0] * 1000) / 1000,
+      trainAcc: Math.round(stats[1].dataSync()[0] * 100 * 100) / 100,
+    });
+    setTrainModelText('Trained');
   }
 
   async function fetching() {
     if (fetchText == 'Fetch') {
       setFetchText('Fetching');
-      //call to get more messages
-      await timeout(800); //for 1 sec delay
-      navigate('/');
+      axios
+        .get('/api/message')
+        .then((res) => {
+          setTrainData(res.data);
+          console.log(trainData[0]);
+          setFetchText('Fetched');
+        })
+        .catch((err) => {
+          console.error(err.toString());
+        });
     }
   }
 
   async function uploading() {
     if (uploadText == 'Upload' && model) {
+      if (trainData.length === 0 || !model) return;
       setUploadText('Uploading');
-      //await timeout(3000).then(() => setUploadText('Uploaded'));
-      //call to upload model
-      //await model.save(window.location.origin + '/api/model', head);
       await model.save(
         tf.io.http(window.location.origin + '/api/model', {
           requestInit: {
             method: 'POST',
-            headers: {
-              numMessages: 130,
-              trainLoss: 1.03,
-              trainAcc: 94.32,
-            },
+            headers: trainStats,
           },
         })
       );
@@ -140,6 +183,15 @@ function Settings() {
             <PulseLoader size={5} color={'white'} />
           ) : null}
           {fetchModelText == 'Fetched' ? <FaCheckCircle></FaCheckCircle> : null}
+        </button>
+      </div>
+      <div className="settingsCard">
+        <button className="cardAction" onClick={train}>
+          {trainModelText} &nbsp;
+          {trainModelText == 'Training' ? (
+            <PulseLoader size={5} color={'white'} />
+          ) : null}
+          {trainModelText == 'Trained' ? <FaCheckCircle></FaCheckCircle> : null}
         </button>
       </div>
     </div>
