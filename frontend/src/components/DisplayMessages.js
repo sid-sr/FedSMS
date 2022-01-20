70192d7f8e6d577658e70e49cc4d9a789755a8d7
Sneha Sriram Kannan <52703275+snehask7@users.noreply.github.com>
2022-01-20 19:12:44 +0530

    route on click of the message

--- frontend/src/components/DisplayMessages.js
+++ frontend/src/components/DisplayMessages.js
@@ -2,88 +2,89 @@
 /* eslint-disable indent */
 / eslint-disable indent/;
 import axios from 'axios';
-import { toast } from 'react-toastify';
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { FaUserCircle } from 'react-icons/fa';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import '../styles/display.css';
-import EachMessage from './EachMessage';
 
 function Display() {
-  const [fetchMessage, setFetchMessage] = useState({
-    message: {
-      someRandomQueryParam: [],
-    },
-  });
+    const [fetchMessage, setFetchMessage] = useState({
+        message: {
+            someRandomQueryParam: [],
+        },
+    });
+    let navigate = useNavigate();
+    const getMessage = () => {
+        axios
+            .get('/api/message')
+            .then((response) => {
+                toast.success('Messages Fetched');
+                setFetchMessage(response.data);
+                // console.log(response.data);
+            })
+            .catch((err) => {
+                toast.error('Error Retrieving Messages!');
+                console.error(err.toString());
+            });
+    };
 
-  const getMessage = () => {
-    axios
-      .get('/api/message')
-      .then((response) => {
-        toast.success('Messages Fetched');
-        setFetchMessage(response.data);
-        // console.log(response.data);
-      })
-      .catch((err) => {
-        toast.error('Error Retrieving Messages!');
-        console.error(err.toString());
-      });
-  };
+    const diplayMessage = (mes) => {
+        console.log(mes)
+        navigate('/eachmessage', { state: { message: 'testing' } });
+    }
+    useEffect(() => {
+        getMessage();
+    }, []);
 
-  useEffect(() => {
-    getMessage();
-  }, []);
-  const data = 'hi';
-
-  return (
-    <div className="container">
-      <a href="/home" className="backText">
-        <IoIosArrowBack style={{ marginBottom: '2px' }}></IoIosArrowBack>
-        <span>Filters</span>
-      </a>
-      <h1 className="settingsHeading">Messages</h1>
-      <hr className="divider" />
-      {/* </div><div className="homeCard">
+    return (
+        <div className="container">
+            <a href="/home" className="backText">
+                <IoIosArrowBack style={{ marginBottom: '2px' }}></IoIosArrowBack>
+                <span>Filters</span>
+            </a>
+            <h1 className="settingsHeading">Messages</h1>
+            <hr className="divider" />
+            {/* </div><div className="homeCard">
         <a href="/" className="nextMessage">
           <FaUserCircle
             size={38}
             style={{ marginBottom: '2px', color: '#D1D3D4' }}
           ></FaUserCircle> */}
-      <div className="homeCard">
-        {fetchMessage
-          ? Object.values(fetchMessage).map((mes, index) => {
-              const short = (mes.message || '').substring(0, 38);
-              return (
-                <div key={index}>
-                  {/* <br /> */}
-                  <a href="/EachMessage" className="nextMessage" key={index}>
-                    <FaUserCircle
-                      size={38}
-                      style={{ marginBottom: '2px', color: '#D1D3D4' }}
-                    ></FaUserCircle>
-                    <span
-                      style={{
-                        marginLeft: '15px',
-                        fontSize: '15px',
-                        color: '#7a7979',
-                        float: 'center',
-                      }}
-                    >
-                      {short}
-                    </span>
-                    <a href="/EachMessage" style={{ marginLeft: '20px' }}></a>
-                    <EachMessage data={data} />
-                    <a href="/EachMessage" className="next">
-                      <IoIosArrowForward></IoIosArrowForward>
-                    </a>
-                  </a>
-                  <hr className="divider2"></hr>
-                </div>
-              );
-            })
-          : null}
-      </div>
-    </div>
-  );
+            <div className="homeCard">
+                {fetchMessage
+                    ? Object.values(fetchMessage).map((mes, index) => {
+                        const short = (mes.message || '').substring(0, 38);
+                        return (
+                            <div key={index} onClick={() => diplayMessage(mes)}>
+                                {/* <br /> */}
+                                <a href="/EachMessage" className="nextMessage" key={index}>
+                                    <FaUserCircle
+                                        size={38}
+                                        style={{ marginBottom: '2px', color: '#D1D3D4' }}
+                                    ></FaUserCircle>
+                                    <span
+                                        style={{
+                                            marginLeft: '15px',
+                                            fontSize: '15px',
+                                            color: '#7a7979',
+                                            float: 'center',
+                                        }}
+                                    >
+                                        {short}
+                                    </span>
+                                    <a href="/EachMessage" className="next">
+                                        <IoIosArrowForward></IoIosArrowForward>
+                                    </a>
+                                </a>
+                                <hr className="divider2"></hr>
+                            </div>
+                        );
+                    })
+                    : null}
+            </div>
+        </div>
+    );
 }
 export default Display;
--- frontend/src/components/EachMessage.js
+++ frontend/src/components/EachMessage.js
@@ -1,26 +1,32 @@
-import { IoIosArrowBack } from 'react-icons/io';
+/* eslint-disable react/jsx-key */
+/* eslint-disable indent */
+/ eslint-disable indent/;
 import { FaUserCircle } from 'react-icons/fa';
+import { IoIosArrowBack } from 'react-icons/io';
+import { useLocation } from 'react-router-dom';
 import '../styles/message.css';
 
-const EachMessage = (props) => {
-  return (
-    <div className="container">
-      <div className="home">
-        <a href="/DisplayMessages" className="back">
-          <IoIosArrowBack style={{ marginBottom: '2px' }}></IoIosArrowBack>
-        </a>
-        <span>{props.data}</span>
-        <FaUserCircle
-          className="Logo"
-          size={50}
-          style={{ marginBottom: '2px', color: '#D1D3D4' }}
-        ></FaUserCircle>
-      </div>
-      <br></br>
-      <br></br>
-      <div className="homeNew"></div>
-    </div>
-  );
+const EachMessage = () => {
+    const { state } = useLocation();
+    return (
+        <div className="container">
+            <div className="home">
+                <a href="/DisplayMessages" className="back">
+                    <IoIosArrowBack style={{ marginBottom: '2px' }}></IoIosArrowBack>
+                </a>
+                <FaUserCircle
+                    className="Logo"
+                    size={50}
+                    style={{ marginBottom: '2px', color: '#D1D3D4' }}
+                ></FaUserCircle>
+            </div>
+            <br></br>
+            <br></br>
+            <div className="homeNew">
+                <span>{state ? state.message : null}</span>
+            </div>
+        </div>
+    );
 };
 
 export default EachMessage;
