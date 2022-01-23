3b75a0f5d4b798fd2031dec9a99accbce07cd685
Sneha Sriram Kannan <52703275+snehask7@users.noreply.github.com>
2022-01-24 02:12:03 +0530

    fetched  from indexed db

--- frontend/src/components/Junk.js
+++ frontend/src/components/Junk.js
@@ -1,32 +1,22 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable indent */
-import axios from 'axios';
-import { toast } from 'react-toastify';
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import db from '../utils/db';
 import Messages from './Messages';
 
 const Junk = () => {
-  const [fetchMessage, setFetchMessage] = useState([]);
+    const [fetchMessage, setFetchMessage] = useState([]);
 
-  const getMessage = () => {
-    axios
-      .get('/api/message')
-      .then((response) => {
-        // Filter messages.
-        toast.success('Messages Fetched');
-        setFetchMessage(response.data);
-      })
-      .catch((err) => {
-        toast.error('Error Retrieving Messages!');
-        console.error(err.toString());
-      });
-  };
+    async function getMessage() {
+        const messages = await db.messages.toArray();
+        setFetchMessage(messages)
+    }
 
-  useEffect(() => {
-    getMessage();
-  }, []);
+    useEffect(() => {
+        getMessage();
+    }, []);
 
-  return <Messages messages={fetchMessage} title="Junk" />;
+    return <Messages messages={fetchMessage} title="Junk" />;
 };
 
 export default Junk;
